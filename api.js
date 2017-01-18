a1938c978c0922d3dcc0bdda1fde047bf511df91
nitaybz <nitaybz@gmail.com>
2017-01-18 19:11:54 +0200

    Update api.js

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 
     app.get('/alarm/check', function(req, res) {
             var statusNow = app.storage.getItem("alarm");
-            res.end(statusNow.value);
+            res.end('"' + statusNow + '"');
     })
 
     app.get('/alarm/arm/:state', function(req, res) {
